2007470020ab8982e846935304d1178dbfc4649a
Fabian Bull <fabian.bull@bioinform.at>
2017-12-05 20:47:52 +0100

    Correct syntax error in notmuch tagging script

--- roles/mail/files/notmuch-tagging.sh
+++ roles/mail/files/notmuch-tagging.sh
@@ -3,14 +3,14 @@ echo "Tagging new mail"
 notmuch new
 
 # Remove orca emails from inbox
-notmuch tag -new -inbox -- to:orca-team@datameer.com
+notmuch tag -inbox -- to:orca-team@datameer.com and topic:JIRA
 
 # tag all messages from "me" as sent and remove tags inbox and unread
-notmuch tag -new -inbox +sent -- (from:fabian.bull@datameer.com \
+notmuch tag -inbox +sent -- '(from:fabian.bull@datameer.com \
     or from:bull.fabian@googlemail.com \
     or from:fabian.bull@bioinform.at \
     or from:spam@bioinform.at \
     or from:fabian.bull@informatik.uni-halle.de) \
-    and not tag:sent
+    and not tag:sent'
 
 echo "Done"
