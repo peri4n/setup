ff5cc13f2aec4826d011f90d43fdc3eac2f27d57
Fabian Bull <fabian.bull@bioinform.at>
2018-01-14 21:37:37 +0100

    Name workspaces in Xmonad

--- roles/dotfiles/files/xmonad.hs
+++ roles/dotfiles/files/xmonad.hs
@@ -3,11 +3,18 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 
-main = xmonad =<< xmobar defaultConfig { 
-      borderWidth        = 1
+main = xmonad =<< xmobar myConfig
+
+myConfig = defaultConfig {
+      modMask = mod4Mask
+    , borderWidth        = 1
     , terminal           = "urxvt"
     , normalBorderColor  = "#cccccc"
     , focusedBorderColor = "#1e90ff"
+    , workspaces         = myWorkspaces
     , manageHook = manageDocks <+> manageHook defaultConfig
     , layoutHook = avoidStruts  $  layoutHook defaultConfig
     }
+
+myWorkspaces :: [String]
+myWorkspaces = ["Term", "Dev", "Chat", "Mail"]
