645dcf9de5b61c41fc4ac3046a96463a5e5062a0
Fabian Bull <fabian.bull@bioinform.at>
2018-01-15 22:01:58 +0100

    Improvement to make Xmonad work with IntelliJ
    - Follow focus fixes the problem in IntelliJ with little popup window.
    - The WMName is set to fix general problems with swing applications.

--- roles/dotfiles/files/xmonad.hs
+++ roles/dotfiles/files/xmonad.hs
@@ -2,18 +2,21 @@ import XMonad
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
 
 main = xmonad =<< xmobar myConfig
 
 myConfig = defaultConfig {
-      modMask = mod4Mask
-    , borderWidth        = 1
-    , terminal           = "urxvt"
-    , normalBorderColor  = "#cccccc"
-    , focusedBorderColor = "#1e90ff"
-    , workspaces         = myWorkspaces
-    , manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts  $  layoutHook defaultConfig
+    modMask = mod4Mask
+  , borderWidth        = 1
+  , terminal           = "urxvt"
+  , normalBorderColor  = "#cccccc"
+  , focusedBorderColor = "#1e90ff"
+  , focusFollowsMouse = False
+  , startupHook = setWMName "LG3D"
+  , workspaces         = myWorkspaces
+  , manageHook = manageDocks <+> manageHook defaultConfig
+  , layoutHook = avoidStruts  $  layoutHook defaultConfig
     }
 
 myWorkspaces :: [String]
