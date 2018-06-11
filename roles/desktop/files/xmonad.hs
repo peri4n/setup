5dba77a1fd5879e02eb9c6f5886cf76d720cc8f1
Fabian Bull <fabian.bull@bioinform.at>
2018-06-11 20:11:16 +0200

    Improve integration of xmobar

--- roles/desktop/files/xmonad.hs
+++ roles/desktop/files/xmonad.hs
@@ -12,8 +12,7 @@ import XMonad.Prompt.Pass
 import XMonad.Util.Run(spawnPipe)
 
 main = do
-    xmbProc <- spawnPipe "xmobar ~/.config/xmobar/xmobarrc"
-    xmonad $ ewmh def {
+    xmonad =<< statusBar myBar myPP toggleStrutsKey (ewmh def {
     modMask = mod4Mask
   , borderWidth        = 1
   , terminal           = "urxvt"
@@ -23,15 +22,22 @@ main = do
   , focusFollowsMouse  = myFocusFollowsMouse
   , clickJustFocuses   = myClickJustFocuses
   , workspaces         = myWorkspaces
-  , handleEventHook    = handleEventHook defaultConfig <+> docksEventHook
-  , logHook            = dynamicLogWithPP xmobarPP { 
-                          ppOutput = hPutStrLn xmbProc
-                        , ppTitle = xmobarColor "#00FFFF" "" . shorten 50
-                        , ppCurrent = xmobarColor "#00FFFF" ""
-                        }
-  , manageHook         = manageDocks <+> myManageHook <+> manageHook def
+--  , handleEventHook    = handleEventHook defaultConfig <+> docksEventHook
+  , manageHook         =  myManageHook <+> manageHook def
   , layoutHook         = avoidStruts  myLayout
-    }
+                      })
+
+-- Command to launch the bar.
+myBar = "xmobar"
+
+-- Custom PP, configure it as you like. It determines what is being written to the bar.
+myPP = xmobarPP { 
+        ppTitle = xmobarColor "#00FFFF" "" . shorten 50
+      , ppCurrent = xmobarColor "#00FFFF" ""
+                }
+
+-- Key binding to toggle the gap for the bar.
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask .|. shiftMask, xK_b)
 
 -- Click is passed through when clicked on an inactive window
 myClickJustFocuses = False
@@ -44,7 +50,7 @@ myWorkspaces = ["Web", "Dev", "Chat", "Mail"]
 
 myManageHook  = composeAll [ 
   appName  =? "Navigator"       --> doShift "Web" -- firefox
-                          ]
+                           ]
 
 myLayout = maximize (tiled) ||| Mirror tiled ||| Full
     where
@@ -60,49 +66,50 @@ myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     -- launching and killing programs
     [ ((modMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf) -- %! Launch terminal
-    , ((modMask,               xK_p     ), spawn "rofi -show run") -- %! Launch dmenu
-    , ((modMask,               xK_w     ), spawn "rofi -show window") -- %! Launch dmenu
-    , ((modMask .|. shiftMask, xK_c     ), kill) -- %! Close the focused window
+      , ((modMask,               xK_p     ), spawn "rofi -show run") -- %! Launch dmenu
+      , ((modMask,               xK_w     ), spawn "rofi -show window") -- %! Launch dmenu
+      , ((modMask .|. shiftMask, xK_c     ), kill) -- %! Close the focused window
 
-    , ((modMask,               xK_space ), sendMessage NextLayout) -- %! Rotate through the available layout algorithms
-    , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf) -- %!  Reset the layouts on the current workspace to default
+    -- switch layouts
+      , ((modMask,               xK_space ), sendMessage NextLayout) -- %! Rotate through the available layout algorithms
+      , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf) -- %!  Reset the layouts on the current workspace to default
 
-    , ((modMask,               xK_n     ), refresh) -- %! Resize viewed windows to the correct size
+      , ((modMask,               xK_n     ), refresh) -- %! Resize viewed windows to the correct size
 
     -- move focus up or down the window stack
-    , ((modMask,               xK_Tab   ), windows W.focusDown) -- %! Move focus to the next window
-    , ((modMask .|. shiftMask, xK_Tab   ), windows W.focusUp  ) -- %! Move focus to the previous window
-    , ((modMask,               xK_j     ), windows W.focusDown) -- %! Move focus to the next window
-    , ((modMask,               xK_k     ), windows W.focusUp  ) -- %! Move focus to the previous window
-    , ((modMask,               xK_m     ), windows W.focusMaster  ) -- %! Move focus to the master window
+      , ((modMask,               xK_Tab   ), windows W.focusDown) -- %! Move focus to the next window
+      , ((modMask .|. shiftMask, xK_Tab   ), windows W.focusUp  ) -- %! Move focus to the previous window
+      , ((modMask,               xK_j     ), windows W.focusDown) -- %! Move focus to the next window
+      , ((modMask,               xK_k     ), windows W.focusUp  ) -- %! Move focus to the previous window
+      , ((modMask,               xK_m     ), windows W.focusMaster  ) -- %! Move focus to the master window
 
     -- modifying the window order
-    , ((modMask,               xK_Return), windows W.swapMaster) -- %! Swap the focused window and the master window
-    , ((modMask .|. shiftMask, xK_j     ), windows W.swapDown  ) -- %! Swap the focused window with the next window
-    , ((modMask .|. shiftMask, xK_k     ), windows W.swapUp    ) -- %! Swap the focused window with the previous window
+      , ((modMask,               xK_Return), windows W.swapMaster) -- %! Swap the focused window and the master window
+      , ((modMask .|. shiftMask, xK_j     ), windows W.swapDown  ) -- %! Swap the focused window with the next window
+      , ((modMask .|. shiftMask, xK_k     ), windows W.swapUp    ) -- %! Swap the focused window with the previous window
 
     -- resizing the master/slave ratio
-    , ((modMask,               xK_h     ), sendMessage Shrink) -- %! Shrink the master area
-    , ((modMask,               xK_l     ), sendMessage Expand) -- %! Expand the master area
+      , ((modMask,               xK_h     ), sendMessage Shrink) -- %! Shrink the master area
+      , ((modMask,               xK_l     ), sendMessage Expand) -- %! Expand the master area
 
     -- floating layer support
-    , ((modMask,               xK_t     ), withFocused $ windows . W.sink) -- %! Push window back into tiling
+      , ((modMask,               xK_t     ), withFocused $ windows . W.sink) -- %! Push window back into tiling
 
     -- increase or decrease number of windows in the master area
-    , ((modMask              , xK_comma ), sendMessage (IncMasterN 1)) -- %! Increment the number of windows in the master area
-    , ((modMask              , xK_period), sendMessage (IncMasterN (-1))) -- %! Deincrement the number of windows in the master area
+      , ((modMask               , xK_comma ), sendMessage (IncMasterN 1)) -- %! Increment the number of windows in the master area
+      , ((modMask               , xK_period), sendMessage (IncMasterN (-1))) -- %! Deincrement the number of windows in the master area
 
     -- quit, or restart
-    , ((modMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess)) -- %! Quit xmonad
-    , ((modMask              , xK_q     ), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi") -- %! Restart xmonad
+      , ((modMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess)) -- %! Quit xmonad
+      , ((modMask              , xK_q     ), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi") -- %! Restart xmonad
 
     -- pass integration
-    , ((modMask, xK_y)                              , passPrompt def)
+      , ((modMask, xK_y)                              , passPrompt def)
 
     ]
     ++
     -- mod-[1..9] %! Switch to workspace N
     -- mod-shift-[1..9] %! Move client to workspace N
     [((m .|. modMask, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+      | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+      , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
