43284be865e199b87c077192ff463defe0a9a529
Fabian Bull <fabian.bull@bioinform.at>
2019-01-01 20:40:11 +0100

    Remove uberspace email accounts

--- roles/mail/files/notmuch-tagging.sh
+++ roles/mail/files/notmuch-tagging.sh
@@ -3,19 +3,9 @@ echo "Tagging new mail"
 notmuch new
 
 notmuch tag +jira 'subject:[JIRA] from:r2d2@datameer.com'
-notmuch tag +gh 'from:notifications@github.com'
 
 # mark all unimportant emails as read and remove it from the inbox
-notmuch tag -inbox -unread -- 'tag:gh and -@peri4n'
 notmuch tag -inbox -unread -- 'to:orca-team@datameer.com'
 notmuch tag -inbox -unread -- 'to:narwhal-team@datameer.com and tag:jira'
-
-# tag all messages from "me" as sent and remove tags inbox and unread
-notmuch tag -inbox +sent -- '(from:fabian.bull@datameer.com \
-    or from:bull.fabian@googlemail.com \
-    or from:fabian.bull@bioinform.at \
-    or from:spam@bioinform.at \
-    or from:fabian.bull@informatik.uni-halle.de) \
-    and not tag:sent'
 
 echo "Done"
